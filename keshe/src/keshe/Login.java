8e40217c032f39c77694998bca231bb3670ea335
bjfubljx <3488731470@qq.com>
2019-01-02 22:31:14 +0800

    新增表

--- keshe/src/keshe/Login.java
+++ keshe/src/keshe/Login.java
@@ -3,6 +3,16 @@ package keshe;
 public class Login {
 	private int User_ID;
 	private String User_Name;
+	
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Login(int user_ID, String user_Name) {
+		super();
+		User_ID = user_ID;
+		User_Name = user_Name;
+	}
 	public int getUser_ID() {
 		return User_ID;
 	}
--- keshe/src/text/logintext.java
+++ keshe/src/text/logintext.java
@@ -1,5 +1,62 @@
 package text;
 
-public class logintext {
+import java.sql.SQLException;
+import java.util.List;
 
+import DAo.LoginSQL;
+import keshe.Login;
+public class logintext {
+	public void testInsertlogin() {
+		Login login=new Login(4,"");
+		LoginSQL dao=new LoginSQL();
+		try {
+			dao.addLogin(login);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testUpdatelogin() {
+		Login login=new Login(4,"");
+		LoginSQL dao=new LoginSQL();
+		try {
+			dao.updateLogin(login);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testDeletelogin() {
+		LoginSQL dao=new LoginSQL();
+		try {
+			dao.deleteLogin(9);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testGetLoginById() {
+		LoginSQL dao=new LoginSQL();
+		try {
+			Login login=dao.findLogin(3);
+			System.out.println("Loginid: "+login.getUser_ID());
+			System.out.println("LoginName: "+ login.getUser_Name());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testGetAllActor() {
+		LoginSQL dao=new LoginSQL();
+		try {
+			List<Login> logins=dao.getAllLogin();
+			for(int i=0;i<logins.size();i++)
+			{
+				System.out.println("Loginid: "+logins.get(i).getUser_ID());
+				System.out.println("LoginName: "+ logins.get(i).getUser_Name());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
